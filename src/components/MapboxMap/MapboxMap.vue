25320281865170939deedc23dc623b5dd0f2ace2
Oleksandr Pikush <pikush.oleksandr@gmail.com>
2025-11-21 12:44:47 +0200

    Update MapboxMap.vue

--- src/components/MapboxMap/MapboxMap.vue
+++ src/components/MapboxMap/MapboxMap.vue
@@ -2,6 +2,7 @@
 import { MapboxMap, MapboxMarker } from '@studiometa/vue-mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import { mapboxSettings } from '@/config/mapbox.js'
+import { ref } from 'vue'
 
 defineProps({
   places: {
@@ -10,9 +11,15 @@ defineProps({
   },
 })
 
+const mapMarkerLngLat = ref(null)
+
 const emit = defineEmits(['marker-click'])
 const setActiveId = (id) => {
   emit('marker-click', id)
+}
+
+const handleMapClick = ({ lngLat }) => {
+  mapMarkerLngLat.value = [lngLat.lng, lngLat.lat]
 }
 </script>
 
@@ -23,8 +30,14 @@ const setActiveId = (id) => {
     :mapStyle="mapboxSettings.style"
     :center="mapboxSettings.center"
     :zoom="mapboxSettings.zoom"
+    @mb-click="handleMapClick"
   >
-    <MapboxMarker v-for="place in places" :key="place.id" :lng-lat="place.lngLat">
+    <MapboxMarker v-if="mapMarkerLngLat" :lngLat="mapMarkerLngLat" anchor="bottom">
+      <button @click="setActiveId(place.id)">
+        <img src="/src/assets/img/map-pin-small.svg" alt="Map Pin" />
+      </button>
+    </MapboxMarker>
+    <MapboxMarker v-for="place in places" :key="place.id" :lng-lat="place.lngLat" anchor="bottom">
       <button @click="setActiveId(place.id)">
         <img src="/src/assets/img/map-pin-small.svg" alt="Map Pin" />
       </button>
